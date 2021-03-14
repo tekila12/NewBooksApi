39c5d771c7b804612635401ea0200af443035abc
Ajnur Radovic <ajnur.radovic1@gmail.com>
2021-03-14 15:53:29 +0100

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const importData = require("./data.json")
 const app= express();
-
+const cors=require('cors');
 let port = process.env.PORT || 3000;
-
+app.use(cors());
 app.get("/", (req,res)=>{
-    res.send("Hello")
+    res.send("Hello11")
 })
 
 app.get("/books",(req, res)=>{
