68525ba6ca86ddfc4896657cd3bc25278d5e462a
tekila12 <radovic.ajnur96@gmail.com>
2024-08-24 07:43:59 +0200

    Fix: Point redirection to index.js in functions directory

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,34 +1,18 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const path = require("path");
-const fs = require("fs");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
-// Load data from data.json
-const dataPath = path.join(__dirname, "./data.json");
-let importData;
-
-try {
-    importData = JSON.parse(fs.readFileSync(dataPath, "utf8"));
-} catch (error) {
-    console.error("Error loading data:", error);
-    importData = {}; // Default to an empty object if there's an error
-}
+const importData = require("./data.json");
 
 app.get("/", (req, res) => {
-    res.send("Hello23");
+  res.send("Hello");
 });
 
 app.get("/books", (req, res) => {
-    res.send(importData);
+  res.json(importData);
 });
 
-const port = 3000; // Use a fixed port for local testing
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports.handler = serverless(app); // Export the serverless function
+module.exports.handler = serverless(app);
